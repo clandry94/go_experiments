d7168410ff2b388756d4bbcaa1454e2e16512b1e
Conor Landry <clandry94@ufl.edu>
2017-06-27 21:14:00 -0400

    Organize structs

--- tcp_queue.go
+++ tcp_queue.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"github.com/clandry94/go_ds/queue"
 	"math/rand"
 	"net"
-	"time"
 )
 
 type Request struct {
@@ -17,55 +15,42 @@ type Cookie struct {
 	Id int
 }
 
-type Conn struct {
-	Connection net.Conn
-	Id         int
-}
-
-func (c *Conn) Handle() {
-
-	timeout := 5 * time.Second
-	buffReader := bufio.NewReader(c.Connection)
-
-	c.Connection.SetReadDeadline(time.Now().Add(timeout))
-	for {
-
-		// Read tokens delimited by newline
-		bytes, err := buffReader.ReadBytes('\n')
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Printf("%s", bytes)
-	}
-
-	fmt.Printf("%d CLOSING\n", c.Id)
-	c.Connection.Close()
-}
-
 func printQueue(workQueue queue.Queue) {
 	fmt.Printf("==== Queue Size: %v ====\n", workQueue.Size)
-	p := workQueue.Front
-	for p != nil {
-		id := p.Value.(Conn).Id
-		fmt.Println(id)
-		p = p.Next
-	}
+	//p := workQueue.Front
+	//for p != nil {
+	//	id := p.Value.Id
+	//	fmt.Println(id)
+	//	p = p.Next
+	//}
 	fmt.Println()
+}
+
+func worker(work *queue.Queue) {
+	for {
+		if work.Size > 0 {
+			job := work.Front.Value
+			job.Handle()
+			fmt.Println(job)
+			work.Pop()
+		}
+	}
 }
 
 func main() {
 	work := queue.New()
 	fmt.Println("Setting up")
-	ln, err := net.Listen("tcp", ":80")
+	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	go worker(&work)
+
 	for {
 		fmt.Println("listening..")
 		newConnection, err := ln.Accept()
-		conn := Conn{Id: rand.Int(), Connection: newConnection}
+		conn := queue.Conn{Id: rand.Int(), Connection: newConnection}
 		fmt.Printf("Connection received! ID: %v\n", conn.Id)
 		if err != nil {
 			fmt.Println(err)
